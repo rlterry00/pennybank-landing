e57db3ea54d3f07b4aaf9b122d2d4c18a401c456
rlterry00 <chuckterry00@gmail.com>
2021-12-12 03:13:38 -0500

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,17 +5,26 @@ export default function Footer() {
   return (
     <footer>
       <div className="container">
-        <p>&copy; The Blacklight Group 2021. All Rights Reserved.</p>
+        <p>&copy; The Blacklight Group 2022. All Rights Reserved.</p>
         <img src={blight} alt="" className="blight" />
         <ul className="list-inline">
           <li className="list-inline-item">
-            <a href="/#">Privacy</a>
+            <a href="https://www.webapp.pennybank.online/">Account</a>
           </li>
           <li className="list-inline-item">
-            <a href="/#">Terms</a>
+            <a href="/privacy">Privacy</a>
           </li>
           <li className="list-inline-item">
-            <a href="/#">FAQ</a>
+            <a href="/affiliates">Affiliates</a>
+          </li>
+          <li className="list-inline-item">
+            <a href="/faq">FAQ</a>
+          </li>
+          <li className="list-inline-item">
+            <a href="/press">Press</a>
+          </li>
+          <li className="list-inline-item">
+            <a href="/https://blog.pennybank.online/">Blog</a>
           </li>
         </ul>
       </div>
